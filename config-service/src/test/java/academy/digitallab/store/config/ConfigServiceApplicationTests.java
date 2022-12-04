c7df83dc95ada45ffc04b017833875801123ff00
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2022-12-04 12:37:00 -0500

    first commit

---
+++ config-service/src/test/java/academy/digitallab/store/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package academy.digitallab.store.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
